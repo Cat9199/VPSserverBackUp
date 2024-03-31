b5e71e6d95b3d01f9b6213c3e1d0c9f448470ec6
Abdelrahman Nasr <pw91994132.offic@gmail.com>
2024-03-31 15:14:52 +0000

    Automatic backup 2024-03-31 15:14:52

--- git_auto_commit.sh
+++ git_auto_commit.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# Log file
+LOG_FILE="/root/SiteCode/git_auto_commit.log"
+
 # Change directory to your Git repository
 cd /root/SiteCode
+
+# Add timestamp to log
+echo "Script executed at $(date +'%Y-%m-%d %H:%M:%S')" >> "$LOG_FILE"
 
 # Add all changes
 git add .
@@ -10,4 +16,4 @@ git add .
 git commit -m "Automatic backup $(date +'%Y-%m-%d %H:%M:%S')"
 
 # Push changes to remote repository
-git push
+git push >> "$LOG_FILE" 2>&1  # Redirect both stdout and stderr to log file
